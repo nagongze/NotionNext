ad2fabe5bbb012b76aa67d931bf5d996aef447f2
nagongze <contact@nagongze.me>
2023-11-30 11:22:12 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,9 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+  const beamsClient = new PusherPushNotifications.Client({
+    instanceId: 'ffacfd04-1318-4bf3-a236-1ab1c84377f0',
+  });
+
+  beamsClient.start()
+    .then(() => beamsClient.addDeviceInterest('hello'))
+    .then(() => console.log('Successfully registered and subscribed!'))
+    .catch(console.error);
