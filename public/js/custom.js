aa9cfaa4b778bff98b9676c19dd675bab21f8f0d
nagongze <contact@nagongze.me>
2023-11-30 11:32:18 +0800

    upload

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,5 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-import * as PusherPushNotifications from "@pusher/push-notifications-web"
+<script src="https://js.pusher.com/beams/1.0/push-notifications-cdn.js"></script>
 {
   const beamsClient = new PusherPushNotifications.Client({
     instanceId: 'ffacfd04-1318-4bf3-a236-1ab1c84377f0',
